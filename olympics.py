0dded876ca356dfb87a55cbe93f42606917ecaf5
cjskalla <calvin-skalla@uiowa.edu>
2020-08-23 14:01:24 -0500

    Built MMA dataset

--- olympics.py
+++ olympics.py
@@ -6,5 +6,18 @@ import numpy as np
 data = pd.read_csv("data/athlete_events.csv")
 obs_per_sport = data['Sport'].value_counts()
 
-#Medal mapping for classifications
-pd.get_dummies(data, columns=['Sex', 'Medal'])
+#Mapping the medal column to numeric codes for classifying
+medal_mapping = {'Gold': 1, 'Silver': 2, 'Bronze': 3, 0: 0}
+data['Medal'] = data['Medal'].fillna(0)
+data['Medal'] = data['Medal'].map(medal_mapping)
+
+#Extracting MMA sports dataset
+mma = ["Wrestling", "Boxing", "Judo", "Taekwondo", "Weightlifting"]
+mma_data = data[data.Sport.isin(mma)]
+
+#Grouping Countries Medal Counts
+mma_data.groupby('Team')['Medal'].unique()
+
+#Bargraph
+#bar = data.plot.bar(x='Team', y='Medal', rot=0)
+
