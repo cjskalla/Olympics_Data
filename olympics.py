8e10eb5fe6378d0882831e1459095d8651a279cb
cjskalla <calvin-skalla@uiowa.edu>
2020-09-07 09:10:56 -0500

    Changed mma to martial arts

--- olympics.py
+++ olympics.py
@@ -13,44 +13,44 @@ medal_mapping = {'Gold': 1, 'Silver': 2, 'Bronze': 3, 0: 0}
 data['Medal'] = data['Medal'].fillna(0)
 data['Medal'] = data['Medal'].map(medal_mapping)
 
-#Extracting MMA sports dataset
-mma = ["Wrestling", "Boxing", "Judo", "Taekwondo"]
-mma_data = data[data.Sport.isin(mma)]
+#Extracting martial_art sports dataset
+martial_art = ["Wrestling", "Boxing", "Judo", "Taekwondo"]
+martial_art_data = data[data.Sport.isin(martial_art)]
 
 #Creating Gold, Silver, Bronze, and no medal dataframes
-gold_data = mma_data[mma_data.Medal == 1]
-silver_data = mma_data[mma_data.Medal == 2]
-bronze_data = mma_data[mma_data.Medal == 3]
-no_medal_data = mma_data[mma_data.Medal == 0]
+gold_data = martial_art_data[martial_art_data.Medal == 1]
+silver_data = martial_art_data[martial_art_data.Medal == 2]
+bronze_data = martial_art_data[martial_art_data.Medal == 3]
+no_medal_data = martial_art_data[martial_art_data.Medal == 0]
 
 #Calculating totals for each country
-mma_gold = pd.DataFrame(gold_data.groupby('Team')['Medal'].count())
-mma_gold = mma_gold.rename(columns={'Medal': 'Gold'})
+martial_art_gold = pd.DataFrame(gold_data.groupby('Team')['Medal'].count())
+martial_art_gold = martial_art_gold.rename(columns={'Medal': 'Gold'})
 
-mma_silver = pd.DataFrame(silver_data.groupby('Team')['Medal'].count())
-mma_silver = mma_silver.rename(columns={'Medal': 'Silver'})
+martial_art_silver = pd.DataFrame(silver_data.groupby('Team')['Medal'].count())
+martial_art_silver = martial_art_silver.rename(columns={'Medal': 'Silver'})
 
-mma_bronze = pd.DataFrame(bronze_data.groupby('Team')['Medal'].count())
-mma_bronze = mma_bronze.rename(columns={'Medal': 'Bronze'})
+martial_art_bronze = pd.DataFrame(bronze_data.groupby('Team')['Medal'].count())
+martial_art_bronze = martial_art_bronze.rename(columns={'Medal': 'Bronze'})
 
-mma_no_medal = pd.DataFrame(no_medal_data.groupby('Team')['Medal'].count())
-mma_no_medal = mma_no_medal.rename(columns={'Medal': 'NoMedal'})
+martial_art_no_medal = pd.DataFrame(no_medal_data.groupby('Team')['Medal'].count())
+martial_art_no_medal = martial_art_no_medal.rename(columns={'Medal': 'NoMedal'})
 
-#Joining MMA Medal Counts
-mma_medal_count = pd.merge(mma_gold, mma_silver, on='Team')
-mma_medal_count = pd.merge(mma_medal_count, mma_bronze, on='Team')
-mma_medal_count = pd.merge(mma_medal_count, mma_no_medal, on='Team')
-#mma_medal_count = mma_medal_count.assign(Participants=
-                    #mma_medal_count['Gold']+
-                    #mma_medal_count['Silver']+
-                    #mma_medal_count['Bronze']+
-                    #mma_medal_count['NoMedal'])
+#Joining martial_art Medal Counts
+martial_art_medal_count = pd.merge(martial_art_gold, martial_art_silver, on='Team')
+martial_art_medal_count = pd.merge(martial_art_medal_count, martial_art_bronze, on='Team')
+martial_art_medal_count = pd.merge(martial_art_medal_count, martial_art_no_medal, on='Team')
+#martial_art_medal_count = martial_art_medal_count.assign(Participants=
+                    #martial_art_medal_count['Gold']+
+                    #martial_art_medal_count['Silver']+
+                    #martial_art_medal_count['Bronze']+
+                    #martial_art_medal_count['NoMedal'])
 
 #Graph
 colors = ["gold", "silver", "chocolate", "lightblue"]
-mma_sort_data = mma_medal_count.nlargest(15, ['Gold', 'Silver', 'Bronze'])
-mma_sort_data.plot(color=colors, kind='barh', stacked=True)
+martial_art_sort_data = martial_art_medal_count.nlargest(15, ['Gold', 'Silver', 'Bronze'])
+martial_art_sort_data.plot(color=colors, kind='barh', stacked=True)
 plt.ylabel('Team')
 plt.xlabel("Participants")
-plt.title("Mixed Martial Arts Olympic History")
+plt.title("Olympic Martial Arts History")
 plt.show()
